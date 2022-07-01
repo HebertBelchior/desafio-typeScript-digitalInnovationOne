a07617cfd74cf3cab342e47aee9113e41b9a9280
Hebert <hebert.moura@hotmail.com>
2022-06-30 23:58:40 -0300

    meu commit

---
+++ respostaDesafio1.ts
@@ -0,0 +1,10 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+/*let employee = {};
+employee.code = 10;
+employee.name = "John";*/
+
+const funcionario2: {codigo: number, nome: string} = {
+  codigo: 10,
+  nome: 'joao'
+}
